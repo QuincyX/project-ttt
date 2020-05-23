a0a18a60a308a517bed47d564cbfb1fcb028188b
Quincy <likequincy@outlook.com>
2020-05-23 17:57:53 +0800

    feat: add query-all for rule&mock

--- app/controller/mock.ts
+++ app/controller/mock.ts
@@ -28,14 +28,22 @@ export default class extends baseController {
     let { findQuery, pageQuery, sortQuery } = this.ctx.helper.getQuery(
       this.ctx.query
     )
-    pageQuery.total = await this.model.find(findQuery).countDocuments().exec()
-    const list = await this.model
-      .find(findQuery)
-      .sort(sortQuery || '-createAt')
-      .skip(pageQuery.size * (pageQuery.page - 1))
-      .limit(pageQuery.size)
-      .exec()
-    this.success(list, pageQuery)
+    if (pageQuery.size === 0) {
+      const list = await this.model
+        .find(findQuery)
+        .sort(sortQuery || '-createAt')
+        .exec()
+      this.success(list)
+    } else {
+      pageQuery.total = await this.model.find(findQuery).countDocuments().exec()
+      const list = await this.model
+        .find(findQuery)
+        .sort(sortQuery || '-createAt')
+        .skip(pageQuery.size * (pageQuery.page - 1))
+        .limit(pageQuery.size)
+        .exec()
+      this.success(list, pageQuery)
+    }
   }
   public async show() {
     const doc = await this.model.findById(this.ctx.params.id)
